abf7cb395c3db811a4ad5c794dafb1281914df08
Kent Loog <kentloog@Kents-MacBook-Pro.local>
2018-04-18 17:01:37 +0300

    Added face detect API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'da29650f17294fbaae60d75fc9620935'
+});
 
 const particleOptions = {
   particles: {
@@ -19,6 +25,33 @@ const particleOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageURL: ''
+    };
+  }
+
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageURL: this.state.input });
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        // do something with response
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  };
+
   render() {
     return (
       <div className="App">
@@ -26,9 +59,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* {
-        <FaceRecognition />} */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageURL={this.state.imageURL} />
       </div>
     );
   }
